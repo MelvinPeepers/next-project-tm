fc027b2280fdb0ed7d85bc2d37955f883cfcf94a
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-08-20 12:26:39 -0500

    adding image

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div className="text-2xl">
       HomePage
       <img
-        src="https://next-project-tm.netlify.app/images/test.jpg?w=10&h=100&fit=cover&q=5"
+        src="https://next-project-tm.netlify.app/images/test.jpg?w=11&h=11&fit=cover&q=5"
         alt="Your Alt Text"
       />
     </div>
