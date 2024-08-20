f78d7a17424a77ea08e974b6a2268926ba02ee08
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-08-20 12:32:11 -0500

    adding image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    loader: "default", // Disables Next.js image optimization
+  },
+};
 
 export default nextConfig;
