708f034591a42a1b7fbfddff2973a6ed9c227eb8
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-09-11 11:10:50 -0500

    added console log

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,4 +1,19 @@
+// const AddPropertyPage = () => {
+//   return <div>Add Property Page</div>;
+// };
+
+// export default AddPropertyPage;
+
+import { useEffect } from "react";
+
 const AddPropertyPage = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // This ensures the code only runs in a client-side environment
+      console.log("Current route:", window.location.pathname);
+    }
+  }, []);
+
   return <div>Add Property Page</div>;
 };
 
