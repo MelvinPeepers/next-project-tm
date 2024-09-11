fafd281f0e83303fa83b6a41abc38aecd851d238
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-09-11 12:10:05 -0500

    updated properties id page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,22 @@
+"use client";
+
+// const PropertyPage = () => {
+//   return <div>Property Page</div>;
+// };
+
+// export default PropertyPage;
+
+import { useEffect } from "react";
+
 const PropertyPage = () => {
-  return <div>Property Page</div>;
+  useEffect(() => {
+    // Call the function when the component mounts
+    fetch("/.netlify/functions/logRoute")
+      .then((response) => response.json())
+      .then((data) => console.log(data.message));
+  }, []);
+
+  return <div>Property Page ID</div>;
 };
 
 export default PropertyPage;
