ec210b691e9b63e8ff83e08d7acd1673cb7346eb
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-09-20 10:30:12 -0500

    static page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,34 +1,5 @@
-// const PropteriesPage = () => {
-//   return <div>Properties Page</div>;
-// };
-
-// export default PropteriesPage;
-
-import React from "react";
-
-const PropertiesPage = ({ data }) => {
-  return (
-    <div>
-      <h1>Properties Page</h1>
-      <p>This is a static page, pre-rendered at build time.</p>
-      <p>Data: {data}</p>{" "}
-      {/* Displaying some static data passed via getStaticProps */}
-    </div>
-  );
+const PropteriesPage = () => {
+  return <div>Properties Page</div>;
 };
 
-export default PropertiesPage;
-
-// This function gets called at build time on the server-side.
-export async function getStaticProps() {
-  // Here you can simulate fetching data or just provide static data.
-  const data = "Example static data for the properties page.";
-
-  // By returning { props: { data } }, the PropertiesPage component
-  // will receive `data` as a prop at build time
-  return {
-    props: {
-      data,
-    },
-  };
-}
+export default PropteriesPage;
